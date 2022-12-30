07e60a4d38d64ade720cb247aefdbf4537f88abb
linhth317 <linhth@finsify.com>
2022-12-30 18:00:36 +0700

    UI Item Task Card of Home Screen

--- lib/home.dart
+++ lib/home.dart
@@ -40,7 +40,13 @@ class HomeBody extends StatelessWidget {
       children: <Widget>[
         _headerScreen(),
         _titleOverview(),
-        _listOverView()
+        _listOverView(),
+        _titleHighPriority(),
+        SizedBox(height: 16),
+        CardTaskItem(titleTask: "Buy Fruits", contendTask: "XXX",),
+        SizedBox(height: 16),
+        CardTaskItem(titleTask: "Buy Fruits", contendTask: "XXX",),
+
       ],
     );
   }
@@ -55,7 +61,7 @@ Widget _headerScreen() {
       ),
     ),
     height: 200,
-    padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+    padding: const EdgeInsets.only(left: 16.0, top: 48.0),
     child: Row(
       children: <Widget>[_avatar(), _titleHeader(), _goDetailButton()],
     ),
@@ -87,7 +93,7 @@ Widget _goDetailButton() {
   return Expanded(
     child: Container(
       alignment: Alignment.topRight,
-      margin: EdgeInsets.only(right: 16.0),
+      margin: EdgeInsets.only(right: 16.0, top: 24.0),
       child: SvgPicture.asset('assets/ic_back_black.svg'),
     )
   );
@@ -177,42 +183,157 @@ class CardOverView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: new Card(
+      child: Card(
           child:Container(
-            padding: EdgeInsets.only(top: 16.0),
-            width: 200,
-            height: 100,
+            padding: const EdgeInsets.only(left: 32.0, top: 16.0, right: 32.0, bottom: 16.0),
             child:  Column(
               children: <Widget>[
-                Text(taskNumber,
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.brown,
-                    )),
-                Text(contend,
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.brown,
-                    )),
+                Container(
+                  width: 100,
+                  alignment: Alignment.center,
+                  child: Text(taskNumber,
+                      style: const TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown,
+                      )),
+                ),
+                Container(
+                  width: 100,
+                  alignment: Alignment.center,
+                  child:Text(contend,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown,
+                      )),
+                ),
               ],
             ),
           )
       ),
-      decoration: new BoxDecoration(
+      decoration: BoxDecoration(
         boxShadow: [
-          new BoxShadow(
+          BoxShadow(
             color: Colors.grey.withOpacity(0.5), //color of shadow
             spreadRadius: 1, //spread radius
             blurRadius: 8, // blur radius
-            offset: Offset(5, 5), // changes position of shadow
+            offset: const Offset(5, 5), // changes position of shadow
           ),
         ],
       ),
-      margin: EdgeInsets.only(left:8.0, right: 8.0),
+      margin: const EdgeInsets.only(left:8.0, right: 8.0),
 
     );
   }
 
 }
+
+Widget _titleHighPriority() {
+  return Container(
+    alignment: Alignment.centerLeft,
+    margin: EdgeInsets.only(left: 16.0,top: 32.0),
+    child: const Text("High Priority",
+        style: TextStyle(
+          fontSize: 24.0,
+          color: Colors.black,
+        )),
+  );
+}
+
+class CardTaskItem extends StatelessWidget {
+  const CardTaskItem({Key? key, required this.titleTask, required this.contendTask})
+      : super(key: key);
+
+  final String titleTask;
+  final String contendTask;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
+          //set border radius more than 50% of height and width to make circle
+        ),
+          child:Container(
+            width: 340,
+            height: 112,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.only(left: 12.0, top: 8.0),
+                  child: const Text("Buy Fruits",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      )),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.only(left: 12.0, top: 8.0),
+                  child: const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      )),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.only(left: 12.0, top: 8.0),
+                        child: const Text("3",
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            )),
+                      ),
+
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.only(left: 42.0, top: 8.0),
+                        child: const Text("30 Dec 2022",
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+      ),
+
+
+      decoration: BoxDecoration(
+          // border: Border.all(
+          //   color: Colors.red![500],
+          // ),
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), //color of shadow
+            spreadRadius: 1, //spread radius
+            blurRadius: 8, // blur radius
+            offset: const Offset(5, 5), // changes position of shadow
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.only(left:8.0, right: 8.0),
+
+    );
+  }
+
+}
+
