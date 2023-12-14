733fa27347308489a05437a30589ae77c26765bd
linhth8 <linhth8@bidv.com.vn>
2023-12-14 17:22:05 +0700

    Create Custom Search Dialog + Button Submit

---
+++ lib/ui/base_component/app_filter_dialog.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app/ui/base_component/button_primary.dart';
+import 'package:todo_app/ui/base_component/custom_dialog.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppFilterDialog {
+  final String titleHeader;
+  final BuildContext context;
+
+  AppFilterDialog({
+    required this.titleHeader,
+    required this.context,
+  });
+
+  Future show() {
+    String searchValue = "";
+    return CustomDialog().show(
+        context,
+        content: Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24)
+          ),
+          backgroundColor: Colors.white,
+          child: Container(
+            height: 200,
+            child: Column(
+              children: [
+                Padding(padding: const EdgeInsets.only(top: 12, bottom: 24),
+                    child: Text(titleHeader, style: TextStyle(
+                      fontSize: 24,
+                      color: HexColor("#C3A87B")
+                    ))),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Input title Task",
+                        filled: true,
+                        fillColor: HexColor("#F4F6F8"),
+                        hintStyle: const TextStyle(
+                            fontSize: 16.0),
+                        contentPadding: const EdgeInsets.all(15),
+                        enabledBorder: OutlineInputBorder(
+                          // width: 0.0 produces a thin "hairline" border
+                          borderSide:
+                          BorderSide(color: HexColor("#C3A87B"), width: 1.0),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4))),
+                    onChanged: (value) {
+                      print("value: "+value);
+                      searchValue = value;
+                    },
+                  ),
+                ),
+                const Expanded(child: SizedBox()),
+                Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 16.0),
+                  child: ButtonPrimary(
+                      title: 'Summit',
+                      colorCodeText: '#FFFFFF',
+                      colorCodeBackGround: '#C3A87B',
+                      onClick: () {
+                        print("Search value: "+searchValue);
+                        Get.back();
+                      },),
+                )
+              ],
+            ),
+
+          ),
+        ));
+  }
+}
